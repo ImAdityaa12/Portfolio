f2f33610945f52f0ed97ac57252a53e24e7a28e2
ImAdityaa12 <adityagupta1291@gmail.com>
2025-06-02 16:00:59 +0530

    refactor(layout): remove GTM script and noscript fallback
    The Google Tag Manager implementation has been removed from the layout component as it's no longer needed or being managed elsewhere. This cleanup improves code maintainability by removing unused tracking code.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,31 +27,6 @@ export default function RootLayout({
             <body
                 className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
             >
-                {/* GTM Script - moved to body */}
-                <Script
-                    id="gtm-script"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-NWJK729Z');
-                        `,
-                    }}
-                />
-
-                {/* GTM noscript fallback */}
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-NWJK729Z"
-                        height="0"
-                        width="0"
-                        style={{ display: 'none', visibility: 'hidden' }}
-                    />
-                </noscript>
-
                 <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
                 <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
